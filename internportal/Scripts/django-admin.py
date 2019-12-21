a5bb391a4643593e3959596b4ff8f0dcc03d79c0
rishipathak6 <radhakpathak5@gmail.com>
2019-12-21 15:26:40 +0530

    Register page also done

--- internportal/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rishi_rip\documents\intern_fair_portal\internportal\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
